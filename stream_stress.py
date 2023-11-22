a066c7912e42c81b815e93b26bca314cbde62223
shabrina_sulistio <shabrinaal10@gmail.com>
2023-11-22 12:10:00 +0700

    update Streamlit

--- stream_stress.py
+++ stream_stress.py
@@ -18,12 +18,23 @@ Bekerja = st.radio('Apakah Anda bekerja (termasuk kerja paruh waktu)?',
 JamTidurPerhari = st.text_input('Berapa jam Anda tidur dalam sehari? (contoh: 8)')
 KesibukanPerhari = st.text_input('Berapa jam kesibukan Anda dalam sehari? (bulatkan dalam jam)')
 FrequencyOfAnger = st.radio('Dalam sebulan terakhir, seberapa sering Anda kesal karena suatu hal terjadi secara tak terduga?',
-                            ['Tidak pernah', 'Hampir tidak pernah', 'Terkadang', 'Cukup sering', 'Sangat sering']
-                            )
+                            ['Tidak pernah', 'Hampir tidak pernah', 'Terkadang', 'Cukup sering', 'Sangat sering'], key=int)
 FrequencyOfAnger = st.radio('Dalam sebulan terakhir, seberapa sering Anda merasa tidak mampu mengendalikan hal-hal penting dalam hidup Anda?',
-                            ['Tidak pernah', 'Hampir tidak pernah', 'Terkadang', 'Cukup sering', 'Sangat sering']
-                            )
-### Bekerja', 'Jam Tidur Perhari', 'Kesibukan Perhari', 'Frequency of Anger', 'Lack of Control', 'Anxiety and Stress', 'Self-Efficacy', 'Perceived Control', 'Perceived Overwhelm', 'Feeling of Mastery', 'Cumulative Difficulty'
+                            ['Tidak pernah', 'Hampir tidak pernah', 'Terkadang', 'Cukup sering', 'Sangat sering'])
+LackOfControl = st.radio('Dalam sebulan terakhir, seberapa sering Anda merasa gugup dan stres?',
+                         ['Tidak pernah', 'Hampir tidak pernah', 'Terkadang', 'Cukup sering', 'Sangat sering'])
+AnxietyAndStress = st.radio('Dalam sebulan terakhir, seberapa sering Anda merasa yakin dengan kemampuan Anda dalam menangani masalah pribadi?',
+                            ['Tidak pernah', 'Hampir tidak pernah', 'Terkadang', 'Cukup sering', 'Sangat sering'])
+SelfEfficacy = st.radio('Dalam sebulan terakhir, seberapa sering Anda merasa segalanya berjalan sesuai keinginan Anda?', 
+                         ['Tidak pernah', 'Hampir tidak pernah', 'Terkadang', 'Cukup sering', 'Sangat sering'])
+PerceivedControl = st.radio('Dalam sebulan terakhir, seberapa sering Anda menyadari bahwa Anda tidak mampu menyelesaikan semua hal yang harus Anda lakukan?',
+                            ['Tidak pernah', 'Hampir tidak pernah', 'Terkadang', 'Cukup sering', 'Sangat sering'])
+PerceivedOverwhelm = st.radio('Dalam sebulan terakhir, seberapa sering Anda menyadari bahwa Anda tidak mampu menyelesaikan semua hal yang harus Anda lakukan?',
+                             ['Tidak pernah', 'Hampir tidak pernah', 'Terkadang', 'Cukup sering', 'Sangat sering'])
+FeelingOfMastery = st.radio('Dalam sebulan terakhir, seberapa sering Anda merasa berada di puncak segalanya?', 
+                            ['Tidak pernah', 'Hampir tidak pernah', 'Terkadang', 'Cukup sering', 'Sangat sering'])
+CumulativeDifficulty = st.radio('Dalam sebulan terakhir, seberapa sering Anda merasakan kesulitan yang menumpuk sehingga Anda tidak mampu mengatasinya?',
+                                ['Tidak pernah', 'Hampir tidak pernah', 'Terkadang', 'Cukup sering', 'Sangat sering'])
 
 # Proses
 ## Prediksi
